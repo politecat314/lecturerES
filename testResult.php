b0b2aff6194f454b25692bfdc91290db448ef067
Aman <ahmedamanibrahim@gmail.com>
2021-04-29 11:17:17 +0500

    test answer display

--- testResult.php
+++ testResult.php
@@ -1,5 +1,224 @@
 <?php
 print_r($_GET);
-
-
 ?>
+
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/es/connection.php';
+?>
+
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+</head>
+
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="container">
+        <br>
+        <h3>Your result</h3>
+        <br>
+
+
+
+        <?php
+        $conn = OpenCon();
+        // echo "Connected Successfully";
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT question_id, question FROM questions";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+
+            $i = 0;
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                $question_id = $row['question_id'];
+                $question = $row['question'];
+
+                echo '<div>
+                        <p><b>' . ++$i . '. '.$question.' </b></p>
+                        <ul class="list-group">';
+
+                $sql2 = "SELECT * FROM answers WHERE question_id = $question_id";
+                $result2 = $conn->query($sql2);
+
+                $answer_submitted = $_GET[$question_id];
+                // var_dump($result2);
+                if ($result2->num_rows > 0) {
+                    // output data of each row
+                    while ($row2 = $result2->fetch_assoc()) {
+                        $ans_highlight = '';
+
+                        
+
+
+                        $correct = '';
+                        
+                        if ($row2['correct_ans']) {
+                            $correct = 'Correct answer';
+
+                            if ($row2['answers_id'] === $answer_submitted) {
+                                $ans_highlight = 'list-group-item-success';
+                            }
+                            
+
+                        } else if ($row2['answers_id'] === $answer_submitted) { // is this the wrong answer which was submitted?
+                            $ans_highlight = 'list-group-item-danger';
+                        }
+
+                            echo  '
+
+                              <li class="list-group-item d-flex justify-content-between align-items-center '.$ans_highlight.'">
+                              ' . $row2['answer_string'] . '
+                              <span class="badge badge-success badge pill">'.$correct.'</span>
+                              </li>';
+                          
+                    }
+
+                    echo '</ul>
+                    <br><br>
+                </div>';
+                } else {
+                    echo "0 results";
+                }
+            }
+        } else {
+            echo "0 results";
+        }
+        CloseCon($conn);
+
+        ?>
+
+
+
+        <div>
+            <p><b>1. This is the first question</b></p>
+
+
+            <ul class="list-group">
+
+                <li class="list-group-item d-flex justify-content-between align-items-center list-group-item-danger">
+                    Cras justo odio
+                    <span class="badge badge-primary badge-pill">14</span>
+                </li>
+
+
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Dapibus ac facilisis in
+                    <span class="badge badge-primary badge-pill">2</span>
+                </li>
+
+                <li class="list-group-item d-flex justify-content-between align-items-center list-group-item-success">
+                    Morbi leo risus
+                    <span class="badge badge-success badge pill">Correct Answer</span>
+                </li>
+            </ul>
+            <br><br>
+        </div>
+
+        <div>
+            <p><b>1. This is the first question</b></p>
+            <ul class="list-group">
+
+                <li class="list-group-item d-flex justify-content-between align-items-center list-group-item-danger">
+                    Cras justo odio
+                    <span class="badge badge-primary badge-pill">14</span>
+                </li>
+
+
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Dapibus ac facilisis in
+                    <span class="badge badge-primary badge-pill">2</span>
+                </li>
+
+                <li class="list-group-item d-flex justify-content-between align-items-center list-group-item-success">
+                    Morbi leo risus
+                    <span class="badge badge-success badge pill">Correct Answer</span>
+                </li>
+            </ul>
+            <br><br>
+        </div>
+
+        <div>
+            <p><b>1. This is the first question</b></p>
+            <ul class="list-group">
+
+                <li class="list-group-item d-flex justify-content-between align-items-center list-group-item-danger">
+                    Cras justo odio
+                    <span class="badge badge-primary badge-pill">14</span>
+                </li>
+
+
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Dapibus ac facilisis in
+                    <span class="badge badge-primary badge-pill">2</span>
+                </li>
+
+                <li class="list-group-item d-flex justify-content-between align-items-center list-group-item-success">
+                    Morbi leo risus
+                    <span class="badge badge-success badge pill">Correct Answer</span>
+                </li>
+            </ul>
+            <br><br>
+        </div>
+
+
+
+    </div>
+
+
+    </div>
+
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+</body>
+
+</html>
