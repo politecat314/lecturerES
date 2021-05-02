8a1bc941391ae37586f85a7a6701e38398329b99
kerolzeeq <kerolzeeq@gmail.com>
2021-05-03 01:52:57 +0800

    Changed LOPES to Fun With Java ES

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ include 'connection.php';
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-
 </head>
 
 
@@ -16,7 +15,9 @@ include 'connection.php';
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">LOPES</a>
+            <a class="navbar-brand" href="#">
+            Fun With Java ES
+            </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
--- topics.php
+++ topics.php
@@ -17,7 +17,7 @@ include 'connection.php';
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="index.php">LOPES</a>
+            <a class="navbar-brand" href="index.php">Fun With Java ES</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
