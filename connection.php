b96227d2b9bc8d3657bbe7b096c94020dc903a37
Aman <ahmedamanibrahim@gmail.com>
2021-05-02 15:39:10 +0500

    dbase bug fix

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@ function OpenCon()
  $dbhost = "localhost";
  $dbuser = "root";
  $dbpass = "";
- $db = "fop_lecturer_es.sql";
+ $db = "fop_lecturer_es";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
  
  return $conn;
