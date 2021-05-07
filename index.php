01400233444f995a143a5f7783105a525fa5a09d
kerolzeeq <kerolzeeq@gmail.com>
2021-05-07 14:33:52 +0800

    Added timezone
    Display
    "morning" from 5-11.59
    "afternoon" from 12-16.59
    "evening" from 17-20.59
    "night" from 21-4.59

--- index.php
+++ index.php
@@ -93,7 +93,35 @@ text-decoration: none; }
 
     <div class="container">
         <br>
-        <h3>Good morning and welcome to FOP!</h3>
+        <?php
+        // $dayofweek = date("w");
+    
+        // $today="";
+        // switch ($dayofweek){
+        //     case 0: $today="Sunday"; break;
+        //     case 1: $today="Monday"; break;
+        //     case 2: $today="Tuesday"; break;
+        //     case 3: $today="Wednesday"; break;
+        //     case 4: $today="Thursday"; break;
+        //     case 5: $today="Friday"; break;
+        //     case 6: $today="Saturday"; break;
+        // }
+        // $times = (date("g")+6).":".date("i").":".date("s");
+        // echo "<p>Today is $today $times </p>";
+
+        $currenttime = date("g")+6;
+        $timezone="";
+        if($currenttime>=5 && $currenttime<12)
+            $timezone="morning";
+        else if($currenttime>=12 && $currenttime<17)
+            $timezone="afternoon";
+        else if($currenttime>=17 && $currenttime<21)
+            $timezone="evening";
+        else if($currenttime>=21 && $currenttime<5)
+            $timezone="night";
+        echo "<h3>Good $timezone and welcome to FOP!</h3>"
+        ?>
+        
         <br>
         <!-- <a class="btn btn-success btn-lg btn-block" href="topics.php">Begin Learning</a>
         <a class="btn btn-primary btn-lg btn-block" href="faq.php">FAQ</a>
